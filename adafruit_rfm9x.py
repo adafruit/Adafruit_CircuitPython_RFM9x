cefd778643ac2ee5376b8fda5bd3f729e25b2aef
Adam Leyshon <aleyshon@thecodecache.net>
2021-01-28 19:35:28 +0000

    Add SNR property according to datasheet
    Format with Black

--- adafruit_rfm9x.py
+++ adafruit_rfm9x.py
@@ -12,17 +12,14 @@ http: www.airspayce.com/mikem/arduino/RadioHead/
 
 * Author(s): Tony DiCola, Jerry Needell
 """
-import time
 import random
-from micropython import const
-
+import time
 
 import adafruit_bus_device.spi_device as spidev
-
+from micropython import const
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_RFM9x.git"
-
 
 # Internal constants:
 # Register names (FSK Mode even though we use LoRa instead, from table 85)
@@ -496,6 +493,16 @@ class RFM9x:
         else:
             raw_rssi -= 164
         return raw_rssi
+
+    @property
+    def snr(self):
+        """The SNR (in dB) of the last received message."""
+        # Read SNR 0x19 register and convert to value using formula in datasheet.
+        # SNR(dB) = PacketSnr [twos complement] / 4
+        snr_byte = self._read_u8(_RH_RF95_REG_19_PKT_SNR_VALUE)
+        if snr_byte > 127:
+            snr_byte = (256 - snr_byte) * -1
+        return snr_byte / 4
 
     @property
     def signal_bandwidth(self):
